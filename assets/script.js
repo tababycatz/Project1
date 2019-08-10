27b84042c4c78e0922b4a42a37ba68141813ee36
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-08-10 12:50:52 -0400

    updated script with movie script

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,55 @@ $(document).ready(function(){
     
     $('.parallax').parallax();
 
-    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-        $.ajax({
+    // var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    //     $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //     }).then(function(response) {
+    //     console.log(response);
+    // });
+    
+    // Movie API
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&1c5dcd2a"
+    $.ajax({
         url: queryURL,
         method: "GET"
-        }).then(function(response) {
-        console.log(response);
-    });
+    }).then(function(response) {
     
+     // Creating a div to hold the movie
+     var movieDiv = $("<div class='movie'>");
+
+     // Storing the rating data
+     var rating = response.Rated;
+
+     // Creating an element to have the rating displayed
+     var pOne = $("<p>").text("Rating: " + rating);
+
+     // Displaying the rating
+     movieInfo.append(pOne);
+
+     // Storing the release year
+     var released = response.Released;
+
+     // Creating an element to hold the release year
+     var pTwo = $("<p>").text("Released: " + released);
+
+     // Retrieving the URL for the image
+     var imgURL = response.Poster;
+
+     // Creating an element to hold the image
+     var image = $("<img>").attr("src", imgURL);
+
+     // Appending the image
+     movieGif.append(image);
+
+     // Putting the entire movie above the previous movies
+    //  $("#movies-view").prepend(movieDiv);
+   });
+
+ }   
+    });
+
+
     
 });
