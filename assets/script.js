51ce26ff7e49ae29eea72e29ed88ad2ef48af926
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-08-13 21:42:32 -0400

    update to pull

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     $('.parallax').parallax();
-    
+
     // Movie API //
 
-     $("#searchBtn").on("click", function(event) {
+    $("#searchBtn").on("click", function (event) {
 
         event.preventDefault();
 
@@ -13,17 +13,17 @@ $(document).ready(function(){
         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=1c5dcd2a";
 
         $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
             var movieDiv = $("#movieInfo");
             var imgDiv = $("#movieGif");
 
             var rating = response.Rated;
             var tOne = $("<p>").text("Rating: " + rating);
             movieDiv.append(tOne);
-            
+
             var runtime = response.Runtime;
             var tTwo = $("<p>").text("Runtime: " + runtime);
             movieDiv.append(tTwo);
@@ -39,42 +39,39 @@ $(document).ready(function(){
             var imgURL = response.Poster;
             var image = $("<img>").attr("src", imgURL);
             imgDiv.append(image);
-            
+
         });
 
         $("#movieInfo").empty();
         $("#movieGif").empty();
 
-      });
+    });
 
-// function newCard() {
+    // Cocktail API //
+    
+    $("#searchBtn").on("click", function (event) {
 
-//     // Deleting the movies prior to adding new movies
-//     // (this is necessary otherwise you will have repeat buttons)
-//     $("#movieDump").empty();
+        event.preventDefault();
 
-//     // Looping through the array of movies
-//     for (var i = 0; i < movies.length; i++) {
+       var queryURLONE = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-//       // Then dynamicaly generating buttons for each movie in the array
-//       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-//       var a = $("<div>");
-//       // Adding a class of movie-btn to our button
-//       a.addClass("movieInfo");
-//       // Adding a data-attribute
-//       a.attr("data-name", movies[i]);
-//       // Providing the initial button text
-//       a.text(movies[i]);
-//       // Adding the button to the buttons-view div
-//       $("#movieDump").append(a);
-//     }
-//   }; //newCard function curly end//
+        $.ajax({
+            url: queryURLONE,
+            method: "GET"
+        }).then(function (response) {
+
+            $("#drinkInfo").text(JSON.stringify(response));
+            console.log(response)
+    
+        });
+
+    
+
+    });
+
+    // Contact Form //
 
 
-// // Adding a click event listener to all elements with a class of "movie-btn"
-// $("#searchBtn").on("click", function(event){
-
-// });
 
 }); //document.ready curly end// 
 
